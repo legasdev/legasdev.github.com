cc1ff6d7a2339fa49797fb63dee450eebd9d9199
Artem Stepanov <temka.do@gmail.com>
2018-11-07 12:20:16 +0300

    Update first screen design

--- js/main.js
+++ js/main.js
@@ -1,64 +1,33 @@
 'use strict'
 
 var
-    AboutBlockInfoPos,
-    ScrollDownPage,
-    DetectViewPort;
+    hoverCompassLink;
 
 $(document).ready( () => {
-    ScrollDownPage();
-    $('.main').removeClass('hideOn');
-});
-
-
-$(window).scroll( (e) => {
-	AboutBlockInfoPos();
-    DetectViewPort();
-});
-
-// Отслеживаем положение блока about
-AboutBlockInfoPos = () => {
-    let 
-		obj = $('.about').find('.about-w-i-wrapper'),
-		checkObj = $('.about').find('.about-w-text');
-		
-	if ( 
-		$(checkObj).offset().top < $(window).scrollTop() && 
-	   	$(checkObj).offset().top + $(checkObj).height() - $(obj).height() > $(window).scrollTop()	
-	) {
-		if (!$(obj).hasClass('fixed')) {
-			$(obj).addClass('fixed');
-			$(obj).css('width', $(obj).parent().width());
-		}
-		if ($(obj).hasClass('bottom')) $(obj).removeClass('bottom');
-	} else {
-		$(obj).removeClass('fixed');
-		
-		// Если ушли ниже
-		if ( $(checkObj).offset().top + $(checkObj).height() - $(obj).height() < $(window).scrollTop() ) {
-			$(obj).addClass('bottom');
-		}
-	}
-}
-
-// Скроллинг по стрелочке
-ScrollDownPage = () => {
-    $('body').on('click', '.main-c-bottom', (e) => {
-       $('html, body').animate({scrollTop: $(window).innerHeight()}, 500); 
-    }); 
-}
-
-// Определение видимого блока 
-DetectViewPort = () => {
     
-    // Проходим по всем базовым блокам
-    $('body').children().filter('section').each( (i, e) => {
-        // Проверка на то, был ли виден блок
-        let 
-            screenPosY = $(window).scrollTop() + $(window).height() / 2;
-        
-        if ($(e).hasClass('hideOn') && screenPosY > $(e).offset().top) {
-            $(e).removeClass('hideOn');
-        }
+    $('body').on('mouseenter', '.compass-link', (e) => {
+        hoverCompassLink(e);
     });
+    
+});
+
+// Направление стрелки
+hoverCompassLink = (e) => {
+    switch ($(e.currentTarget).index()) {
+        case 0:
+            $('.main-c-arrow').css('transform', 'rotate(45deg)');
+          break;
+                
+          case 1:
+              $('.main-c-arrow').css('transform', 'rotate(0)');
+         break;
+           
+         case 2:
+             $('.main-c-arrow').css('transform', 'rotate(-45deg)');
+          break;
+                
+          case 3:
+             $('.main-c-arrow').css('transform', 'rotate(-90deg)');
+         break;
+    }
 }
