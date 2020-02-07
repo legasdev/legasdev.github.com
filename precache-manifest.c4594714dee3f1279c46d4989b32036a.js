afc3a3071d841717a922273b4918b97fb968aa88
legasdev <temka.do@gmail.com>
2020-02-07 11:47:05 +0300

    Updates

---
+++ precache-manifest.c4594714dee3f1279c46d4989b32036a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc8fc5592bd25f4060baa316a22c872",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "90db7dab9463f6560bb7",
+    "url": "/test-task/static/css/main.716eeaab.chunk.css"
+  },
+  {
+    "revision": "fde85a15dcbfdeade0c9",
+    "url": "/test-task/static/js/2.3bfb7638.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test-task/static/js/2.3bfb7638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90db7dab9463f6560bb7",
+    "url": "/test-task/static/js/main.d165466a.chunk.js"
+  },
+  {
+    "revision": "9af63af73764d407529d",
+    "url": "/test-task/static/js/runtime-main.418677bd.js"
+  },
+  {
+    "revision": "50a08ca22b4783f0d2b945294f5d6d79",
+    "url": "/test-task/static/media/img1.50a08ca2.png"
+  },
+  {
+    "revision": "e20a9bb9d905b74712091fb24707f885",
+    "url": "/test-task/static/media/img1@2x.e20a9bb9.png"
+  },
+  {
+    "revision": "48ff2dd4835431ca663dfd2f6eaca90b",
+    "url": "/test-task/static/media/img2.48ff2dd4.png"
+  },
+  {
+    "revision": "84feee453e42dd9bf68c5ddfa94e5181",
+    "url": "/test-task/static/media/img2@2x.84feee45.png"
+  },
+  {
+    "revision": "a74fe6844b1453a88b16a3ee104473cf",
+    "url": "/test-task/static/media/img3.a74fe684.png"
+  },
+  {
+    "revision": "27e0aa0174f8f6e6a0c1cb40b4c848b2",
+    "url": "/test-task/static/media/img3@2x.27e0aa01.png"
+  }
+]);
