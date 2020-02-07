b46821f5aaaee9a5c27ab6b8343b1e5bab1616cd
legasdev <temka.do@gmail.com>
2020-02-07 11:48:29 +0300

    fix bugs

--- test-task/service-worker.js
+++ test-task/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f661eb67bd7d60609f2a1cd854b1c9c.js"
+  "/test-task/precache-manifest.c4594714dee3f1279c46d4989b32036a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
